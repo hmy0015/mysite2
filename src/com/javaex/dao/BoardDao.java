e222628c69025b6363df1bdac8c4eaee39c0c784
hi <hi@DESKTOP-A3OJ2BV>
2020-06-29 18:23:49 +0900

    dao 리스트 출력 기능 구현

--- src/com/javaex/dao/BoardDao.java
+++ src/com/javaex/dao/BoardDao.java
@@ -5,6 +5,11 @@ import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+import com.javaex.vo.BoardVo;
+import com.javaex.vo.GuestVo;
 
 public class BoardDao {
 	// import java.sql.*;
@@ -50,13 +55,52 @@ public class BoardDao {
 			System.out.println("error:" + e);
 		}
 	}
-	
+
 	// 리스트
-	
+	public List<BoardVo> getBoardList() {
+		// 리스트 준비
+		List<BoardVo> bList = new ArrayList<BoardVo>();
+		getConnection();
+		
+		try {
+			// 3. SQL문 준비 / 바인딩 / 실행 --> 완성된 sql문을 가져와서 작성할것
+			String query = "";
+			query += " select  no, ";
+			query += "         title, ";
+			query += "         content, ";
+			query += "         hit, ";
+			query += "         reg_date, ";
+			query += "         user_no";
+			query += " from board ";
+
+			pstmt = conn.prepareStatement(query); // 쿼리로 만들기
+
+			rs = pstmt.executeQuery();
+		
+			// 4.결과처리
+			while (rs.next()) {
+				int no = rs.getInt("no");
+				String title = rs.getString("title");
+				String content = rs.getString("content");
+				int hit = rs.getInt("hit");
+				String reg_date = rs.getString("reg_date");
+				int user_no = rs.getInt("user_no");
+
+				BoardVo vo = new BoardVo(no, title, content, hit, reg_date, user_no);
+				bList.add(vo);
+			}
+		} catch (SQLException e) {
+			System.out.println("error:" + e);
+		}
+
+		close();
+		return bList;
+	}
+
 	// 등록
-	
+
 	// 수정
-	
+
 	// 삭제
-	
+
 }
--- src/com/javaex/dao/DaoTest.java
+++ src/com/javaex/dao/DaoTest.java
@@ -3,10 +3,11 @@ package com.javaex.dao;
 import java.util.ArrayList;
 import java.util.List;
 
-import com.javaex.vo.UserVo;
+import com.javaex.vo.BoardVo;
 
 public class DaoTest {
 	public static void main(String[] args) {
+		/*
 		UserDao dao = new UserDao();
 		
 		//UserVo vo = new UserVo("hi06", "1234", "황민영", "female");
@@ -19,5 +20,16 @@ public class DaoTest {
 			System.out.println("No : " + uvo.getNo() + ", id : " + uvo.getId() + ", password : " + uvo.getPassword()
 								+ ", name : " + uvo.getName() + ", gender : " + uvo.getGender());
 		}
+		*/
+		
+		BoardDao dao = new BoardDao();
+		List<BoardVo> list =  new ArrayList<BoardVo>();
+		list = dao.getBoardList();
+		
+		for(BoardVo vo : list) {
+			System.out.println("No : " + vo.getNo() + ", title : " + vo.getTitle() + ", content : " + vo.getContent()
+								+ ", hit : " + vo.getHit() + ", reg_date : " + vo.getReg_date() + ", user_no : " + vo.getUser_no());
+		}
+		
 	}
 }
